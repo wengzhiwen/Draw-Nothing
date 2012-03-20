25afd3ac9ea515af15c168111e457e70976c8810
Sutra Zhou <zhoushuqun@gmail.com>
2012-03-21 01:21:57 +0800

    Remove useless lines in the output of dictionary query result.

--- src/main/java/net/wengs/drawnothing/SdcvTranslater.java
+++ src/main/java/net/wengs/drawnothing/SdcvTranslater.java
@@ -2,8 +2,10 @@ package net.wengs.drawnothing;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.SystemUtils;
 
 /**
  * @author Sutra Zhou
@@ -19,7 +21,15 @@ public class SdcvTranslater implements Translater {
 					new String[] { "/usr/local/bin/sdcv", "-n", "-u",
 							"XDICT英汉辞典", word });
 			InputStream input = p.getInputStream();
-			return IOUtils.toString(input);
+			List<String> lines = IOUtils.readLines(input);
+			StringBuilder sb = new StringBuilder();
+			for (int i = 1, l = lines.size(); i < l; i++) {
+				String line = lines.get(i);
+				if (!line.startsWith("-->")) {
+					sb.append(line).append(SystemUtils.LINE_SEPARATOR);
+				}
+			}
+			return sb.toString();
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
